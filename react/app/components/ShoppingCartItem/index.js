12fffdf73bffd6fdc6346f9dbb2b6501397b7077
Ryan Wilson <rww9008@rit.edu>
2016-10-07 10:33:31 -0400

    Implemented basic unstyled shopping cart. Not hooked up to sagas or anything

--- react/app/components/ShoppingCartItem/index.js
+++ react/app/components/ShoppingCartItem/index.js
@@ -12,9 +12,20 @@ class ShoppingCartItem extends React.Component { // eslint-disable-line react/pr
   render() {
     return (
       <div className={styles.shoppingCartItem}>
+        <div>{this.props.name}</div>
+        <div>{this.props.details}</div>
+        <div>Quantity: {this.props.quantity}</div>
+        <div>Unit Price: {this.props.unitPrice}</div>
       </div>
     );
   }
 }
+
+ShoppingCartItem.propTypes = {
+  name: React.PropTypes.string,
+  details: React.PropTypes.string,
+  quantity: React.PropTypes.number,
+  unitPrice: React.PropTypes.number,
+};
 
 export default ShoppingCartItem;
--- react/app/containers/SalesAssociatePage/index.js
+++ react/app/containers/SalesAssociatePage/index.js
@@ -11,14 +11,22 @@ import styles from './styles.css';
 
 import ItemOrderForm from 'components/ItemOrderForm';
 import PaymentForm from 'components/PaymentForm';
+import ShoppingCart from 'components/ShoppingCart';
 
 export class SalesAssociatePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const items = [
+      { name: 'fashionW', details: 'Lights up', quantity: 2, unitPrice: 30.05 },
+      { name: 'healthyW', details: 'Lights up', quantity: 2, unitPrice: 50.05 },
+      { name: 'ActiveW', details: 'Lights up', quantity: 2, unitPrice: 500.05 },
+    ];
     return (
       <div className={styles.salesAssociatePage}>
         <h1>Sales Associate</h1>
         <ItemOrderForm getOrderInfo={(obj) => console.log('order obj: ', obj)} />
         <PaymentForm getPaymentInfo={(obj) => console.log('object: ', obj)} />
+        <h2>Shopping Cart</h2>
+        <ShoppingCart items={items} />
       </div>
     );
   }
