d1e33655a4e2ab81306ed0e9981e668d638e40f5
Beetee14 <bjt2654@rit.edu>
2016-10-09 18:45:08 -0400

    Started laying out the structure of the API calls.

--- CustomerSupportAPI/src/CustomerSupportApp.java
+++ CustomerSupportAPI/src/CustomerSupportApp.java
@@ -1,11 +1,38 @@
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 
 public class CustomerSupportApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		get("/goodbye", (req, res) -> "Goodbye World");
+
+		/* Accounting gets return refunds from customer support
+		 * GET
+		 *
+		 */
+		get("/refund", (req, res) -> {
+			float refund = 0;
+			res.type("text/json");
+			return "{\"refund\":\"" + refund + "\"}";
+		});
+		
+		/* Manufacturing sends Customer Support a repaired wearable
+		 * POST
+		 *
+		 */
+		post("/sendrepair", (req, res) -> {
+			return "";
+		});
+		
+		/* Accounting retrieves all general expenses from Silos
+		 * GET
+		 *
+		 */
+		get("/customerSupport/expenses", (req, res) -> {
+			float expenses = 0;
+			res.type("text/json");
+			return "{\"expenses\":\"" + expenses + "\"}";
+		});
 	}
 
 }
--- SalesAPI/src/SalesApp.java
+++ SalesAPI/src/SalesApp.java
@@ -3,7 +3,48 @@ public class SalesApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		get("/hello", (req, res) -> "Hello World");
+		
+		/* Accounting gets revenue from Sales
+		 * GET
+		 *
+		 */
+		get("/revenue", (req, res) -> {
+			float revenue = 0;
+			res.type("text/json");
+			return "{\"revenue\":\"" + revenue + "\"}";
+		});
+		
+		/* Customer Support gets wearable object from Sales database
+		 * GET
+		 *
+		 */
+		get("/getSale/:pid", (req, res) -> {
+			String pid = req.params(":pid"); 
+			return pid;
+		});
+		
+		/* Accounting retrieves all general expenses from Silos
+		 * GET
+		 *
+		 */
+		get("/sales/expenses", (req, res) -> {
+			float expenses = 0;
+			res.type("text/json");
+			return "{\"expenses\":\"" + expenses + "\"}";
+		});
+		
+		/* HR gets employee ID and value of commissions for salespersons made weekly
+		 * GET
+		 *
+		 */
+		get("/commissions/:eid", (req, res) -> {
+			String eid = req.params(":eid");
+			float commission = 0;
+			res.type("text/json");
+			return "{\"commission\":\"" + commission + "\"}";
+		});
+		
+		
 	}
 
 }
