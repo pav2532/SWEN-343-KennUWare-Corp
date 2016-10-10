46921dffb7d12fb5e68e79d6cc33bd217fd6fe84
Ryan Wilson <rww9008@rit.edu>
2016-10-10 15:28:10 -0400

    Implemented Employee and Region data classes. Added mappings to hibernate and initial data in import.sql

--- Sales/src/main/resources/import.sql
+++ Sales/src/main/resources/import.sql
@@ -1,4 +1,8 @@
-insert into employee (name, type, password) values ("SalesRep1", "Associate", "test");
-insert into employee (name, type, password) values ("RegionalManager1", "RegionalManager", "test");
-insert into employee (name, type, password) values ("TheGeneral", "GeneralManager", "test");
-insert into employee (name, type, password) values ("SalesRep2", "Associate", "test");
+# Region ids are auto-generated
+insert into region (name) values ("Northeast");
+insert into region (name) values ("Southeast");
+
+insert into employee (name, type, password, regionId) values ("SalesRep1", 0, "test", 1);
+insert into employee (name, type, password, regionId) values ("RegionalManager1", 1, "test", 1);
+insert into employee (name, type, password, regionId) values ("TheGeneral", 2, "test", 1);
+insert into employee (name, type, password, regionId) values ("SalesRep2", 0, "test", 1);
