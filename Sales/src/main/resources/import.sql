de4dfed1c6db85234781946fae44944b0f95587d
Beetee14 <bjt2654@rit.edu>
2016-10-10 16:07:32 -0400

    Created ItemOrders and database mapping

--- Sales/src/main/resources/import.sql
+++ Sales/src/main/resources/import.sql
@@ -3,3 +3,4 @@ insert into employee (name, type, password) values ("RegionalManager1", "Regiona
 insert into employee (name, type, password) values ("TheGeneral", "GeneralManager", "test");
 insert into employee (name, type, password) values ("SalesRep2", "Associate", "test");
 insert into Item (name, unitPrice) values ("Fitness", "10.0");
+insert into ItemOrders (orderId, itemId, quantity) values ("1", "1", "10");
