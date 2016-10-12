8e1d7d4ec8446a75cda8343aa6a45b8e2e02a0b6
Norwal <jfk1307@g.rit.edu>
2016-10-12 00:34:43 -0400

    Skeleton of HTTP call to another group is completed

--- Sales/src/main/java/com/kennuware/sales/services/OrderServices.java
+++ Sales/src/main/java/com/kennuware/sales/services/OrderServices.java
@@ -8,9 +8,17 @@ import com.google.gson.Gson;
 import com.kennuware.sales.domain.ItemOrders;
 import com.kennuware.sales.domain.ShoppingCart;
 import com.kennuware.sales.domain.Wearables.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.hibernate.Session;
 import com.kennuware.sales.domain.SalesOrder;
 import org.hibernate.criterion.Order;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 
 public class OrderServices {
@@ -67,7 +75,27 @@ public class OrderServices {
     public static String sendOrder(SalesOrder order, Session session){
         Gson gson = new Gson();
 
-        //
+        /*try {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost postRequest = new HttpPost(
+                    "http://localhost:8080/RESTfulExample/json/product/post");  //Edit this to be the correct url
+
+            StringEntity input = null;
+                input = new StringEntity(gson.toJson(order));
+
+            input.setContentType("application/json");
+            postRequest.setEntity(input);
+
+            HttpResponse response = httpClient.execute(postRequest); //Do something with response when we know what they respond with
+
+            httpClient.getConnectionManager().shutdown();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
 
         return gson.toJson(order);
     }
