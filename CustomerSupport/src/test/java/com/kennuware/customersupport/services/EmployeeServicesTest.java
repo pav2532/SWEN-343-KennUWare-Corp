682dc55f46415c5ef7f8411619491d48c7a703b0
Ryan Wilson <rww9008@rit.edu>
2016-10-11 22:31:07 -0400

    Smoke test

---
+++ CustomerSupport/src/test/java/com/kennuware/customersupport/services/EmployeeServicesTest.java
@@ -0,0 +1,16 @@
+package com.kennuware.customersupport.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Ryan on 10/11/2016.
+ */
+public class EmployeeServicesTest {
+
+    @Test
+    public void testSmoke() {
+        assertEquals(1,1);
+    }
+}
