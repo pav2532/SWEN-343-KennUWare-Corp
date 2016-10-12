615fbca79b619d889a1e43282cba2f2513aaf146
Norwal <jfk1307@g.rit.edu>
2016-10-11 22:10:55 -0400

    Fixed some package issues

--- Sales/src/test/TestRunner.java
+++ Sales/src/test/TestRunner.java
@@ -2,7 +2,9 @@
  * Created by John King on 11-Oct-16.
  */
 
-import com.kennuware.customersupport.data.HibernateUtil;
+package test;
+
+import com.kennuware.sales.data.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.runner.JUnitCore;
@@ -11,6 +13,10 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
     public static void main(String[] args) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
         Result result = JUnitCore.runClasses(TestUnit.class);
 
         for (Failure failure : result.getFailures()) {
---
+++ Sales/src/test/TestUnit.java
@@ -0,0 +1,17 @@
+/**
+ * Created by John King on 11-Oct-16.
+ */
+
+package test;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
+public class TestUnit {
+
+    @Test
+    public void testingTest(){
+
+    }
+}
