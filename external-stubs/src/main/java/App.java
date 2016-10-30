cf94797c91d8d9fd321f9e2540144d9204cba6c5
Norwal <jfk1307@g.rit.edu>
2016-10-30 14:55:27 -0400

    Started implementation of Human Resources API stubs

--- external-stubs/src/main/java/App.java
+++ external-stubs/src/main/java/App.java
@@ -18,5 +18,21 @@ public class App {
         get("/test", (req, res) -> {
             return new TestObject();
         }, gson::toJson);
+
+        get("verifySalesEID/" + eid + "/", (req, res) -> {
+            double revenue = 0;
+            String rid = req.params(":rid");
+            revenue = EmployeeServices.verifySalesEmployee();
+            res.type("text/json");
+            return "{\"revenue\":\"" + revenue + "\"}";
+        });
+
+        get("verifyCustomerSupportEID/" + eid + "/", (req, res) -> {
+            double revenue = 0;
+            String rid = req.params(":rid");
+            revenue = EmployeeServices.verifyCustomerSupportEmployee();
+            res.type("text/json");
+            return "{\"revenue\":\"" + revenue + "\"}";
+        });
     }
 }
