568c8884750165a39da357cb7f0da5c929ffb769
Norwal <jfk1307@g.rit.edu>
2016-10-30 14:55:45 -0400

    Started implementation of Human Resources API stubs

---
+++ external-stubs/src/main/java/EmployeeServices.java
@@ -0,0 +1,12 @@
+/**
+ * Created by John King on 30-Oct-16.
+ */
+public class EmployeeServices {
+    public static boolean verifyCustomerSupportEmployee(int eid){
+
+    }
+
+    public static boolean verifySalesEmployee(int eid){
+
+    }
+}
